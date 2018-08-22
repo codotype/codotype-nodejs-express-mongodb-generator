2a13a23c7f55358c34346ead8dbbed75503f835d
Alexander Schwartzberg <aeksco@gmail.com>
2018-08-21 23:44:16 -0400

    Updated to be compliant with updated codotype runtime

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const { Runtime } = require('codotype-generator')
+const Codotype = require('codotype-generator')
+
+const app = require('codotype-generator/examples/todo-list.json')
+
+const build = {
+  stages: [{
+    project_path: 'web_api', // TODO - pull this from the generator
+    generator_path: './generator', // TODO - pull this from codotype-meta.json, potentially refactor this approach?
+    configuration: {}, // TODO - this will be populated by the UI
+  }]
+}
 
 // Invoke runtime directly with parameters
 // TODO - this should be removed and replaced by a CLI command in packge.json
-new Runtime({
-  project_path: 'web_api', // TODO - pull default from codotype-meta.json
-  generator_path: './generator', // TODO - pull this from codotype-meta.json, potentially refactor this approach?
-  generator_options: {}, // TODO - this will be populated by the UI
-  app: require('codotype-generator/examples/todo-list.json')
-}).execute()
+// TODO - refactor this so these options are passed into the `execute` method of this instance
+const runtime = new Codotype.runtime()
+
+// Executes the build
+runtime.execute({ app, build })
 
 // NOTE - in-progress generator metadata structure
 // QUESTION - should this be encapsulated in package.json? ...probably not
