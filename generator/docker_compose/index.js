cfd9ad9f8537f1756860e703c003a057031fdd80
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-09 14:41:30 -0400

    Updated and enabled docker-compose generator

--- generator/docker_compose/index.js
+++ generator/docker_compose/index.js
@@ -1,27 +1,25 @@
 const Generator = require('@codotype/generator')
 
+// // // //
+
 module.exports = class DockerCompose extends Generator {
-  async write ({ app }) {
-
-    // Returns boolean wether or not there is seed data
-    // TODO - this is repeated in more than one place
-    // it should be abstracted into some shared location
-    function hasSeedData () {
-      let hasSeeds = false
-      app.seed_data.forEach((s) => {
-        if (s.records.length) {
-          hasSeeds = true
-        }
-      })
-      return hasSeeds
-    }
-
-    // dest/docker-compose-dev.yml
+  async write () {
     await this.copyTemplate(
-      this.templatePath('docker-compose-dev.yml'),
-      this.destinationPath('docker-compose.yml'),
-      { container_name_prefix: app.identifier, seedData: hasSeedData() }
+      this.templatePath('docker-compose.yml'),
+      this.destinationPath('docker-compose.yml')
     )
-
   }
 }
+
+// Returns boolean wether or not there is seed data
+// TODO - this is repeated in more than one place
+// it should be abstracted into some shared location
+// function hasSeedData () {
+//   let hasSeeds = false
+//   app.seed_data.forEach((s) => {
+//     if (s.records.length) {
+//       hasSeeds = true
+//     }
+//   })
+//   return hasSeeds
+// }
