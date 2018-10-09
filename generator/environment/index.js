27e9e94f06b44fe3e4fce54f847805d4fdedb538
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-09 14:56:51 -0400

    Updated Environment generator to produce .env files for docker and local deplos

--- generator/environment/index.js
+++ generator/environment/index.js
@@ -5,8 +5,13 @@ const Generator = require('@codotype/generator')
 module.exports = class EnvironmentGenerator extends Generator {
   async write () {
     await this.copyTemplate(
-      this.templatePath('environment'),
+      this.templatePath('env-dev.txt'),
       this.destinationPath('.env')
+    )
+
+    await this.copyTemplate(
+      this.templatePath('env-docker.txt'),
+      this.destinationPath('.env.docker')
     )
   }
 }
